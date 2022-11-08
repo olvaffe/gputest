8ad10528887c416978d39cb4a2462dc88486e6f3
Chia-I Wu <olvaffe@gmail.com>
2022-11-07 18:24:11 -0800

    dump some usage and feature bits in strings

--- formats.c
+++ formats.c
@@ -12,6 +12,11 @@ struct formats_test_format {
     const char *name;
 };
 
+struct formats_test_name {
+    uint64_t bits;
+    const char *name;
+};
+
 static const struct formats_test_format formats_test_formats[] = {
 #define FMT(fmt)                                                                                 \
     {                                                                                            \
@@ -280,6 +285,64 @@ static const VkImageUsageFlags formats_test_usages[] = {
     VK_IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT,
 };
 
+static const struct formats_test_name formats_test_usage_names[] = {
+    { VK_IMAGE_USAGE_SAMPLED_BIT, "sampled" },
+    { VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT, "color" },
+    { VK_IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT, "depth" },
+};
+
+static const struct formats_test_name formats_test_feature_names[] = {
+    { VK_FORMAT_FEATURE_SAMPLED_IMAGE_BIT, "sampled" },
+    { VK_FORMAT_FEATURE_COLOR_ATTACHMENT_BIT, "color" },
+    { VK_FORMAT_FEATURE_DEPTH_STENCIL_ATTACHMENT_BIT, "depth" },
+    { VK_FORMAT_FEATURE_TRANSFER_SRC_BIT, "xfers" },
+    { VK_FORMAT_FEATURE_TRANSFER_DST_BIT, "xferd" },
+    { VK_FORMAT_FEATURE_MIDPOINT_CHROMA_SAMPLES_BIT, "midpoint" },
+    { VK_FORMAT_FEATURE_SAMPLED_IMAGE_YCBCR_CONVERSION_LINEAR_FILTER_BIT, "linear" },
+    { VK_FORMAT_FEATURE_SAMPLED_IMAGE_YCBCR_CONVERSION_SEPARATE_RECONSTRUCTION_FILTER_BIT,
+      "separate" },
+    { VK_FORMAT_FEATURE_SAMPLED_IMAGE_YCBCR_CONVERSION_CHROMA_RECONSTRUCTION_EXPLICIT_BIT,
+      "explicit" },
+    { VK_FORMAT_FEATURE_SAMPLED_IMAGE_YCBCR_CONVERSION_CHROMA_RECONSTRUCTION_EXPLICIT_FORCEABLE_BIT,
+      "forceable" },
+    { VK_FORMAT_FEATURE_COSITED_CHROMA_SAMPLES_BIT, "cosited" },
+};
+
+static void
+formats_get_str(
+    uint64_t bits, const struct formats_test_name *names, uint32_t count, char *str, size_t size)
+{
+    int len = 0;
+    for (uint32_t i = 0; i < count; i++) {
+        const struct formats_test_name *name = &names[i];
+        if (bits & name->bits) {
+            len += snprintf(str + len, size - len, "%s|", name->name);
+            bits &= ~name->bits;
+        }
+    }
+
+    if (bits)
+        snprintf(str + len, size - len, "0x%" PRIx64, bits);
+    else if (len)
+        str[len - 1] = '\0';
+    else
+        snprintf(str + len, size - len, "none");
+}
+
+static void
+formats_get_usage_str(VkImageUsageFlags usage, char *str, size_t size)
+{
+    formats_get_str(usage, formats_test_usage_names, ARRAY_SIZE(formats_test_usage_names), str,
+                    size);
+}
+
+static void
+formats_get_feature_str(VkFormatFeatureFlags features, char *str, size_t size)
+{
+    formats_get_str(features, formats_test_feature_names, ARRAY_SIZE(formats_test_feature_names),
+                    str, size);
+}
+
 static void
 formats_test_dump_image_format(struct vk *vk,
                                VkFormat format,
@@ -314,15 +377,20 @@ formats_test_dump_image_format(struct vk *vk,
         .sType = VK_STRUCTURE_TYPE_EXTERNAL_IMAGE_FORMAT_PROPERTIES,
         .pNext = &ahb_props,
     };
+    VkSamplerYcbcrConversionImageFormatProperties ycbcr_props = {
+        .sType = VK_STRUCTURE_TYPE_SAMPLER_YCBCR_CONVERSION_IMAGE_FORMAT_PROPERTIES,
+        .pNext = &external_props,
+    };
     VkImageFormatProperties2 props = {
         .sType = VK_STRUCTURE_TYPE_IMAGE_FORMAT_PROPERTIES_2,
-        .pNext = &external_props,
+        .pNext = &ycbcr_props,
     };
 
     const VkResult result =
         vk->GetPhysicalDeviceImageFormatProperties2(vk->physical_dev, &info, &props);
     if (result == VK_SUCCESS) {
-        vk_log("    supported: true");
+        vk_log("    supported: true (desc count %d)",
+               ycbcr_props.combinedImageSamplerDescriptorCount);
 
         if (handle) {
             const VkExternalMemoryProperties *mem_props =
@@ -365,8 +433,12 @@ formats_test_dump_format(struct vk *vk, VkFormat format)
                          mod_props.drmFormatModifierCount;
     vk_log("  supported: %s", can_buffer || can_img ? "true" : "false");
 
-    if (can_buffer)
-        vk_log("  bufferFeatures 0x%08x", props.formatProperties.bufferFeatures);
+    char features_str[128];
+    if (can_buffer) {
+        formats_get_feature_str(props.formatProperties.bufferFeatures, features_str,
+                                ARRAY_SIZE(features_str));
+        vk_log("  bufferFeatures: %s", features_str);
+    }
 
     if (!can_img) {
         if (!can_buffer)
@@ -374,13 +446,20 @@ formats_test_dump_format(struct vk *vk, VkFormat format)
         return;
     }
 
-    vk_log("  linearTilingFeatures 0x%08x", props.formatProperties.linearTilingFeatures);
-    vk_log("  optimalTilingFeatures 0x%08x", props.formatProperties.optimalTilingFeatures);
+    formats_get_feature_str(props.formatProperties.linearTilingFeatures, features_str,
+                            ARRAY_SIZE(features_str));
+    vk_log("  linearTilingFeatures: %s", features_str);
+
+    formats_get_feature_str(props.formatProperties.optimalTilingFeatures, features_str,
+                            ARRAY_SIZE(features_str));
+    vk_log("  optimalTilingFeatures: %s", features_str);
 
     for (uint32_t i = 0; i < mod_props.drmFormatModifierCount; i++) {
         const VkDrmFormatModifierPropertiesEXT *p = &mod_props.pDrmFormatModifierProperties[i];
-        vk_log("  modifier 0x%016" PRIx64 ": 0x%08x plane count %d", p->drmFormatModifier,
-               p->drmFormatModifierTilingFeatures, p->drmFormatModifierPlaneCount);
+        formats_get_feature_str(p->drmFormatModifierTilingFeatures, features_str,
+                                ARRAY_SIZE(features_str));
+        vk_log("  modifier 0x%016" PRIx64 ": %s plane count %d", p->drmFormatModifier,
+               features_str, p->drmFormatModifierPlaneCount);
     }
 
     for (uint32_t h = 0; h < ARRAY_SIZE(formats_test_handles); h++) {
@@ -390,9 +469,12 @@ formats_test_dump_format(struct vk *vk, VkFormat format)
                 const VkImageTiling tiling = formats_test_tilings[t];
                 const VkImageUsageFlags usage = formats_test_usages[u];
 
+                char usage_str[128];
+                formats_get_usage_str(usage, usage_str, ARRAY_SIZE(usage_str));
+
                 if (tiling == VK_IMAGE_TILING_OPTIMAL || tiling == VK_IMAGE_TILING_LINEAR) {
-                    vk_log("  external handle 0x%x, image tiling %s, usage 0x%08x", handle,
-                           tiling == VK_IMAGE_TILING_OPTIMAL ? "optimal" : "linear", usage);
+                    vk_log("  external handle 0x%x, image tiling %s, usage %s", handle,
+                           tiling == VK_IMAGE_TILING_OPTIMAL ? "optimal" : "linear", usage_str);
                     formats_test_dump_image_format(vk, format, handle, tiling,
                                                    DRM_FORMAT_MOD_INVALID, usage);
                 } else {
@@ -400,8 +482,8 @@ formats_test_dump_format(struct vk *vk, VkFormat format)
                         const VkDrmFormatModifierPropertiesEXT *p =
                             &mod_props.pDrmFormatModifierProperties[i];
                         vk_log("  external handle 0x%x, image modifier 0x%016" PRIx64
-                               ", usage 0x%08x",
-                               handle, p->drmFormatModifier, usage);
+                               ", usage %s",
+                               handle, p->drmFormatModifier, usage_str);
                         formats_test_dump_image_format(vk, format, handle,
                                                        VK_IMAGE_TILING_DRM_FORMAT_MODIFIER_EXT,
                                                        p->drmFormatModifier, usage);
