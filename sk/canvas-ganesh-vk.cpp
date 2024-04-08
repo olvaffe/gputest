91c56a0062ab0d824dd685e9f32242e5f927f8e8
Chia-I Wu <olvaffe@gmail.com>
2024-04-08 11:49:37 -0700

    skutil_vk: use vkutil

--- sk/canvas-ganesh-vk.cpp
+++ sk/canvas-ganesh-vk.cpp
@@ -5,13 +5,16 @@
 
 #include "skutil.h"
 #include "skutil_vk.h"
+#include "vkutil.h"
 
 struct canvas_ganesh_vk_test {
     uint32_t width;
     uint32_t height;
 
+    struct vk vk;
     struct sk sk;
-    struct sk_vk vk;
+
+    std::unique_ptr<sk_vk_backend_context> backend_ctx;
     sk_sp<GrDirectContext> ctx;
     sk_sp<SkSurface> surf;
 };
@@ -19,27 +22,27 @@ struct canvas_ganesh_vk_test {
 static void
 canvas_ganesh_vk_test_init(struct canvas_ganesh_vk_test *test)
 {
+    struct vk *vk = &test->vk;
     struct sk *sk = &test->sk;
-    struct sk_vk *vk = &test->vk;
 
+    vk_init(vk, NULL);
     sk_init(sk, NULL);
-    sk_vk_init(vk);
 
-    const GrVkBackendContext backend = sk_vk_make_backend_context(vk);
-    test->ctx = sk_create_context_ganesh_vk(sk, backend);
+    test->backend_ctx = std::make_unique<sk_vk_backend_context>(vk);
+    test->ctx = sk_create_context_ganesh_vk(sk, test->backend_ctx->get());
     test->surf = sk_create_surface_ganesh(sk, test->ctx, test->width, test->height);
 }
 
 static void
 canvas_ganesh_vk_test_cleanup(struct canvas_ganesh_vk_test *test)
 {
+    struct vk *vk = &test->vk;
     struct sk *sk = &test->sk;
-    struct sk_vk *vk = &test->vk;
 
     test->surf.reset();
     test->ctx.reset();
-    sk_vk_cleanup(vk);
     sk_cleanup(sk);
+    vk_cleanup(vk);
 }
 
 static void
--- sk/image-ganesh-vk.cpp
+++ sk/image-ganesh-vk.cpp
@@ -5,13 +5,16 @@
 
 #include "skutil.h"
 #include "skutil_vk.h"
+#include "vkutil.h"
 
 struct image_ganesh_vk_test {
     bool upload;
     const char *filename;
 
+    struct vk vk;
     struct sk sk;
-    struct sk_vk vk;
+
+    std::unique_ptr<sk_vk_backend_context> backend_ctx;
     sk_sp<GrDirectContext> ctx;
 
     sk_sp<SkImage> img;
@@ -21,14 +24,14 @@ struct image_ganesh_vk_test {
 static void
 image_ganesh_vk_test_init(struct image_ganesh_vk_test *test)
 {
+    struct vk *vk = &test->vk;
     struct sk *sk = &test->sk;
-    struct sk_vk *vk = &test->vk;
 
+    vk_init(vk, NULL);
     sk_init(sk, NULL);
-    sk_vk_init(vk);
 
-    const GrVkBackendContext backend = sk_vk_make_backend_context(vk);
-    test->ctx = sk_create_context_ganesh_vk(sk, backend);
+    test->backend_ctx = std::make_unique<sk_vk_backend_context>(vk);
+    test->ctx = sk_create_context_ganesh_vk(sk, test->backend_ctx->get());
 
     test->img = sk_load_png(sk, test->filename);
     assert(!test->img->isTextureBacked());
@@ -52,14 +55,14 @@ image_ganesh_vk_test_init(struct image_ganesh_vk_test *test)
 static void
 image_ganesh_vk_test_cleanup(struct image_ganesh_vk_test *test)
 {
+    struct vk *vk = &test->vk;
     struct sk *sk = &test->sk;
-    struct sk_vk *vk = &test->vk;
 
     test->surf.reset();
     test->img.reset();
     test->ctx.reset();
-    sk_vk_cleanup(vk);
     sk_cleanup(sk);
+    vk_cleanup(vk);
 }
 
 static void
