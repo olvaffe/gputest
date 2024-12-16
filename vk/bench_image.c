0ac4255c5d95220220eeb6c31aa88a3055dfe4e9
Chia-I Wu <olvaffe@gmail.com>
2024-12-15 21:06:10 -0800

    vk/bench_image: let ssbo copy

--- vk/bench_image.c
+++ vk/bench_image.c
@@ -23,6 +23,8 @@ struct bench_image_test {
     uint32_t height;
     uint32_t loop;
 
+    uint32_t cs_local_size;
+
     struct vk vk;
     struct vk_stopwatch *stopwatch;
 };
@@ -195,7 +197,9 @@ bench_image_test_copy(struct bench_image_test *test, struct vk_image *dst, struc
 }
 
 static uint64_t
-bench_image_test_dispatch(struct bench_image_test *test, struct vk_image *img)
+bench_image_test_dispatch(struct bench_image_test *test,
+                          struct vk_image *dst,
+                          struct vk_image *src)
 {
     struct vk *vk = &test->vk;
     struct vk_pipeline *pipeline;
@@ -210,6 +214,12 @@ bench_image_test_dispatch(struct bench_image_test *test, struct vk_image *img)
         const VkDescriptorSetLayoutBinding bindings[] = {
             [0] = {
                 .binding = 0,
+                .descriptorType = VK_DESCRIPTOR_TYPE_STORAGE_IMAGE,
+                .descriptorCount = 1,
+                .stageFlags = VK_SHADER_STAGE_COMPUTE_BIT,
+            },
+            [1] = {
+                .binding = 1,
                 .descriptorType = VK_DESCRIPTOR_TYPE_STORAGE_IMAGE,
                 .descriptorCount = 1,
                 .stageFlags = VK_SHADER_STAGE_COMPUTE_BIT,
@@ -237,7 +247,18 @@ bench_image_test_dispatch(struct bench_image_test *test, struct vk_image *img)
                 .descriptorCount = 1,
                 .descriptorType = VK_DESCRIPTOR_TYPE_STORAGE_IMAGE,
                 .pImageInfo = &(VkDescriptorImageInfo){
-                    .imageView = img->render_view,
+                    .imageView = dst->render_view,
+                    .imageLayout = VK_IMAGE_LAYOUT_GENERAL,
+                },
+            },
+            [1] = {
+                .sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET,
+                .dstSet = set->set,
+                .dstBinding = 1,
+                .descriptorCount = 1,
+                .descriptorType = VK_DESCRIPTOR_TYPE_STORAGE_IMAGE,
+                .pImageInfo = &(VkDescriptorImageInfo){
+                    .imageView = src->render_view,
                     .imageLayout = VK_IMAGE_LAYOUT_GENERAL,
                 },
             },
@@ -250,25 +271,35 @@ bench_image_test_dispatch(struct bench_image_test *test, struct vk_image *img)
         .levelCount = 1,
         .layerCount = 1,
     };
-    const VkImageMemoryBarrier barrier = {
-        .sType = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER,
-        .srcAccessMask = 0,
-        .dstAccessMask = VK_ACCESS_SHADER_WRITE_BIT,
-        .oldLayout = VK_IMAGE_LAYOUT_UNDEFINED,
-        .newLayout = VK_IMAGE_LAYOUT_GENERAL,
-        .image = img->img,
-        .subresourceRange = subres_range,
+    const VkImageMemoryBarrier barriers[2] = {
+        [0] = {
+            .sType = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER,
+            .srcAccessMask = 0,
+            .dstAccessMask = VK_ACCESS_SHADER_WRITE_BIT,
+            .oldLayout = VK_IMAGE_LAYOUT_UNDEFINED,
+            .newLayout = VK_IMAGE_LAYOUT_GENERAL,
+            .image = dst->img,
+            .subresourceRange = subres_range,
+        },
+        [1] = {
+            .sType = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER,
+            .srcAccessMask = 0,
+            .dstAccessMask = VK_ACCESS_SHADER_READ_BIT,
+            .oldLayout = VK_IMAGE_LAYOUT_UNDEFINED,
+            .newLayout = VK_IMAGE_LAYOUT_GENERAL,
+            .image = src->img,
+            .subresourceRange = subres_range,
+        },
     };
 
-    const uint32_t local_size = 8;
-    assert((test->width | test->height) % local_size == 0);
-    const uint32_t group_count_x = test->width / local_size;
-    const uint32_t group_count_y = test->height / local_size;
+    assert((test->width | test->height) % test->cs_local_size == 0);
+    const uint32_t group_count_x = test->width / test->cs_local_size;
+    const uint32_t group_count_y = test->height / test->cs_local_size;
 
     VkCommandBuffer cmd = vk_begin_cmd(vk, false);
     vk->CmdPipelineBarrier(cmd, VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT,
-                           VK_PIPELINE_STAGE_COMPUTE_SHADER_BIT, 0, 0, NULL, 0, NULL, 1,
-                           &barrier);
+                           VK_PIPELINE_STAGE_COMPUTE_SHADER_BIT, 0, 0, NULL, 0, NULL,
+                           ARRAY_SIZE(barriers), barriers);
     vk->CmdBindPipeline(cmd, VK_PIPELINE_BIND_POINT_COMPUTE, pipeline->pipeline);
     vk->CmdBindDescriptorSets(cmd, VK_PIPELINE_BIND_POINT_COMPUTE, pipeline->pipeline_layout, 0,
                               1, &set->set, 0, NULL);
@@ -508,12 +539,15 @@ bench_image_test_draw_ssbo(struct bench_image_test *test, VkImageTiling tiling)
         if (!(mt_mask & (1 << i)))
             continue;
 
-        struct vk_image *img = vk_create_image_with_mt(vk, &info, i);
-        vk_create_image_render_view(vk, img, VK_IMAGE_ASPECT_COLOR_BIT);
+        struct vk_image *dst = vk_create_image_with_mt(vk, &info, i);
+        struct vk_image *src = vk_create_image_with_mt(vk, &info, i);
+        vk_create_image_render_view(vk, dst, VK_IMAGE_ASPECT_COLOR_BIT);
+        vk_create_image_render_view(vk, src, VK_IMAGE_ASPECT_COLOR_BIT);
 
-        const uint64_t dur = bench_image_test_dispatch(test, img);
+        const uint64_t dur = bench_image_test_dispatch(test, dst, src);
 
-        vk_destroy_image(vk, img);
+        vk_destroy_image(vk, dst);
+        vk_destroy_image(vk, src);
 
         vk_log("%s: SSBO: %d MB/s", bench_image_test_describe_mt(test, tiling, i, desc),
                bench_image_test_calc_throughput_mb(test, dur));
@@ -582,6 +616,8 @@ main(int argc, char **argv)
         .width = 1920,
         .height = 1080,
         .loop = 32,
+
+        .cs_local_size = 8,
     };
 
     bench_image_test_init(&test);
