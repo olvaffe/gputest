5314bef64d5bed1f6128d31cd725b19b48d786e7
Chia-I Wu <olvaffe@gmail.com>
2026-01-07 17:27:17 -0800

    vk/android: gpu rendering

--- vk/android.c
+++ vk/android.c
@@ -3,6 +3,8 @@
  * SPDX-License-Identifier: MIT
  */
 
+#include "vkutil.h"
+
 #include <android/choreographer.h>
 #include <android/hardware_buffer.h>
 #include <android/input.h>
@@ -11,9 +13,6 @@
 #include <android/native_activity.h>
 #include <android/native_window.h>
 #include <android/surface_control.h>
-#include <stdarg.h>
-#include <stdlib.h>
-#include <string.h>
 #include <threads.h>
 
 enum android_test_looper_ident {
@@ -27,7 +26,9 @@ struct android_test_state {
 
 struct android_test {
     bool verbose;
-    enum AHardwareBuffer_Format format;
+    VkFormat vk_format;
+    enum AHardwareBuffer_Format ahb_format;
+    uint64_t ahb_usage;
     ANativeActivity *act;
 
     mtx_t mutex;
@@ -37,9 +38,11 @@ struct android_test {
 
     ALooper *looper;
     AChoreographer *choreo;
+
+    struct vk vk;
+
     struct android_test_state cur;
     struct android_test_state next;
-
     ASurfaceControl *ctrl;
 };
 
@@ -64,27 +67,171 @@ android_die(const char *format, ...)
 }
 
 static void
-android_test_handle_frame(int64_t ts, void *arg)
+android_test_ahb_draw_gpu(struct android_test *test, VkImage img)
 {
-    struct android_test *test = arg;
+    const bool protected = test->ahb_usage & AHARDWAREBUFFER_USAGE_PROTECTED_CONTENT;
+    struct vk *vk = &test->vk;
 
-    if (test->verbose)
-        android_log("frame: %" PRIi64, ts);
-
-    if (!test->ctrl)
-        return;
-
-    const AHardwareBuffer_Desc desc = {
-        .width = ANativeWindow_getWidth(test->cur.win),
-        .height = ANativeWindow_getHeight(test->cur.win),
-        .layers = 1,
-        .format = test->format,
-        .usage = AHARDWAREBUFFER_USAGE_CPU_WRITE_RARELY |
-                 AHARDWAREBUFFER_USAGE_GPU_SAMPLED_IMAGE | AHARDWAREBUFFER_USAGE_COMPOSER_OVERLAY,
+    VkCommandBuffer cmd = vk_begin_cmd(vk, protected);
+    const VkImageSubresourceRange subres_range = {
+        .aspectMask = VK_IMAGE_ASPECT_COLOR_BIT,
+        .levelCount = 1,
+        .layerCount = 1,
     };
-    AHardwareBuffer *ahb;
-    if (AHardwareBuffer_allocate(&desc, &ahb))
-        android_die("failed to alloc ahb");
+    const VkImageMemoryBarrier barrier1 = {
+        .sType = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER,
+        .srcAccessMask = 0,
+        .dstAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT,
+        .oldLayout = VK_IMAGE_LAYOUT_UNDEFINED,
+        .newLayout = VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,
+        .srcQueueFamilyIndex = VK_QUEUE_FAMILY_FOREIGN_EXT,
+        .dstQueueFamilyIndex = vk->queue_family_index,
+        .image = img,
+        .subresourceRange = subres_range,
+    };
+    const VkImageMemoryBarrier barrier2 = {
+        .sType = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER,
+        .srcAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT,
+        .dstAccessMask = 0,
+        .oldLayout = VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,
+        .newLayout = VK_IMAGE_LAYOUT_GENERAL,
+        .srcQueueFamilyIndex = vk->queue_family_index,
+        .dstQueueFamilyIndex = VK_QUEUE_FAMILY_FOREIGN_EXT,
+        .image = img,
+        .subresourceRange = subres_range,
+    };
+
+    vk->CmdPipelineBarrier(cmd, VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT, VK_PIPELINE_STAGE_TRANSFER_BIT,
+                           0, 0, NULL, 0, NULL, 1, &barrier1);
+
+    VkClearColorValue clear_val = {
+        .float32 = { 0.5f, 0.5f, 0.5f, 1.0f },
+    };
+    clear_val.float32[protected ? 1 : 2] = 1.0f;
+
+    vk->CmdClearColorImage(cmd, img, barrier1.newLayout, &clear_val, 1, &subres_range);
+
+    vk->CmdPipelineBarrier(cmd, VK_PIPELINE_STAGE_TRANSFER_BIT,
+                           VK_PIPELINE_STAGE_BOTTOM_OF_PIPE_BIT, 0, 0, NULL, 0, NULL, 1,
+                           &barrier2);
+    vk_end_cmd(vk);
+    vk_wait(vk);
+}
+
+static VkDeviceMemory
+android_test_ahb_create_memory(struct android_test *test, AHardwareBuffer *ahb, VkImage img)
+{
+    struct vk *vk = &test->vk;
+
+    VkAndroidHardwareBufferPropertiesANDROID props = {
+        .sType = VK_STRUCTURE_TYPE_ANDROID_HARDWARE_BUFFER_PROPERTIES_ANDROID,
+    };
+    vk->GetAndroidHardwareBufferPropertiesANDROID(vk->dev, ahb, &props);
+
+    const uint32_t mt = ffs(props.memoryTypeBits) - 1;
+
+    const VkImportAndroidHardwareBufferInfoANDROID import_info = {
+        .sType = VK_STRUCTURE_TYPE_IMPORT_ANDROID_HARDWARE_BUFFER_INFO_ANDROID,
+        .buffer = ahb,
+    };
+    const VkMemoryDedicatedAllocateInfo dedicated_info = {
+        .sType = VK_STRUCTURE_TYPE_MEMORY_DEDICATED_ALLOCATE_INFO,
+        .pNext = &import_info,
+        .image = img,
+    };
+    const VkMemoryAllocateInfo alloc_info = {
+        .sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO,
+        .pNext = &dedicated_info,
+        .allocationSize = props.allocationSize,
+        .memoryTypeIndex = mt,
+    };
+
+    VkDeviceMemory mem;
+    vk->result = vk->AllocateMemory(vk->dev, &alloc_info, NULL, &mem);
+    vk_check(vk, "failed to import ahb");
+
+    vk->result = vk->BindImageMemory(vk->dev, img, mem, 0);
+    vk_check(vk, "failed to bind image memory");
+
+    return mem;
+}
+
+static VkImage
+android_test_ahb_create_image(struct android_test *test, AHardwareBuffer *ahb)
+{
+    struct vk *vk = &test->vk;
+
+    AHardwareBuffer_Desc desc;
+    AHardwareBuffer_describe(ahb, &desc);
+
+    const VkImageCreateFlags img_flags = test->ahb_usage & AHARDWAREBUFFER_USAGE_PROTECTED_CONTENT
+                                             ? VK_IMAGE_CREATE_PROTECTED_BIT
+                                             : 0;
+
+    const VkPhysicalDeviceExternalImageFormatInfo fmt_ext_info = {
+        .sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_EXTERNAL_IMAGE_FORMAT_INFO,
+        .handleType = VK_EXTERNAL_MEMORY_HANDLE_TYPE_ANDROID_HARDWARE_BUFFER_BIT_ANDROID,
+    };
+    const VkPhysicalDeviceImageFormatInfo2 fmt_info = {
+        .sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_IMAGE_FORMAT_INFO_2,
+        .pNext = &fmt_ext_info,
+        .format = test->vk_format,
+        .type = VK_IMAGE_TYPE_2D,
+        .tiling = VK_IMAGE_TILING_OPTIMAL,
+        .usage = VK_IMAGE_USAGE_TRANSFER_DST_BIT,
+        .flags = img_flags,
+    };
+    VkExternalImageFormatProperties fmt_ext_props = {
+        .sType = VK_STRUCTURE_TYPE_EXTERNAL_IMAGE_FORMAT_PROPERTIES,
+    };
+    VkImageFormatProperties2 fmt_props = {
+        .sType = VK_STRUCTURE_TYPE_IMAGE_FORMAT_PROPERTIES_2,
+        .pNext = &fmt_ext_props,
+    };
+    vk->result =
+        vk->GetPhysicalDeviceImageFormatProperties2(vk->physical_dev, &fmt_info, &fmt_props);
+    vk_check(vk, "unsupported image");
+
+    const VkExternalMemoryFeatureFlags ext_mem_feats =
+        fmt_ext_props.externalMemoryProperties.externalMemoryFeatures;
+    if (!(ext_mem_feats & VK_EXTERNAL_MEMORY_FEATURE_IMPORTABLE_BIT))
+        vk_die("image does not support import");
+
+    const VkExternalMemoryImageCreateInfo external_info = {
+        .sType = VK_STRUCTURE_TYPE_EXTERNAL_MEMORY_IMAGE_CREATE_INFO,
+        .handleTypes = VK_EXTERNAL_MEMORY_HANDLE_TYPE_ANDROID_HARDWARE_BUFFER_BIT_ANDROID,
+    };
+    const VkImageCreateInfo info = {
+        .sType = VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO,
+        .pNext = &external_info,
+        .flags = img_flags,
+        .imageType = VK_IMAGE_TYPE_2D,
+        .format = test->vk_format,
+        .extent = {
+            .width = desc.width,
+            .height = desc.height,
+            .depth = 1,
+        },
+        .mipLevels = 1,
+        .arrayLayers = 1,
+        .samples = VK_SAMPLE_COUNT_1_BIT,
+        .tiling = VK_IMAGE_TILING_OPTIMAL,
+        .usage = VK_IMAGE_USAGE_TRANSFER_DST_BIT,
+        .initialLayout = VK_IMAGE_LAYOUT_UNDEFINED,
+    };
+
+    VkImage img;
+    vk->result = vk->CreateImage(vk->dev, &info, NULL, &img);
+    vk_check(vk, "failed to create image");
+
+    return img;
+}
+
+static void
+android_test_ahb_draw_cpu(struct android_test *test, AHardwareBuffer *ahb)
+{
+    AHardwareBuffer_Desc desc;
+    AHardwareBuffer_describe(ahb, &desc);
 
     void *ptr;
     int32_t cpp;
@@ -107,6 +254,57 @@ android_test_handle_frame(int64_t ts, void *arg)
         } else {
             memset(row, 0x80, cpp * desc.width);
         }
+    }
+}
+
+static AHardwareBuffer *
+android_test_ahb_alloc(struct android_test *test, uint32_t width, uint32_t height)
+{
+    const AHardwareBuffer_Desc desc = {
+        .width = width,
+        .height = height,
+        .layers = 1,
+        .format = test->ahb_format,
+        .usage = test->ahb_usage,
+    };
+
+    AHardwareBuffer *ahb;
+    if (AHardwareBuffer_allocate(&desc, &ahb))
+        android_die("failed to alloc ahb");
+
+    return ahb;
+}
+
+static void
+android_test_handle_frame(int64_t ts, void *arg)
+{
+    struct android_test *test = arg;
+    struct vk *vk = &test->vk;
+
+    if (!test->ctrl)
+        return;
+
+    const uint32_t width = ANativeWindow_getWidth(test->cur.win);
+    const uint32_t height = ANativeWindow_getHeight(test->cur.win);
+
+    if (test->verbose) {
+        android_log("frame: ts %" PRIi64 ", %dx%d, format 0x%x, usage 0x%" PRIx64, ts, width,
+                    height, test->ahb_format, test->ahb_usage);
+    }
+
+    AHardwareBuffer *ahb = android_test_ahb_alloc(test, width, height);
+
+    if (test->ahb_usage & AHARDWAREBUFFER_USAGE_GPU_FRAMEBUFFER) {
+        VkImage img = android_test_ahb_create_image(test, ahb);
+        VkDeviceMemory mem = android_test_ahb_create_memory(test, ahb, img);
+
+        android_test_ahb_draw_gpu(test, img);
+
+        vk->FreeMemory(vk->dev, mem, NULL);
+        vk->DestroyImage(vk->dev, img, NULL);
+    } else if ((test->ahb_usage & AHARDWAREBUFFER_USAGE_CPU_WRITE_MASK) !=
+               AHARDWAREBUFFER_USAGE_CPU_WRITE_NEVER) {
+        android_test_ahb_draw_cpu(test, ahb);
     }
 
     ASurfaceTransaction *xact = ASurfaceTransaction_create();
@@ -174,6 +372,7 @@ static int
 android_test_thread(void *arg)
 {
     struct android_test *test = arg;
+    struct vk *vk = &test->vk;
 
     mtx_lock(&test->mutex);
 
@@ -184,6 +383,17 @@ android_test_thread(void *arg)
     test->choreo = AChoreographer_getInstance();
     if (!test->choreo)
         android_die("failed to get choreographer");
+
+    const char *const dev_exts[] = {
+        VK_ANDROID_EXTERNAL_MEMORY_ANDROID_HARDWARE_BUFFER_EXTENSION_NAME,
+        VK_EXT_QUEUE_FAMILY_FOREIGN_EXTENSION_NAME,
+    };
+    const struct vk_init_params vk_params = {
+        .protected_memory = test->ahb_usage & AHARDWAREBUFFER_USAGE_PROTECTED_CONTENT,
+        .dev_exts = dev_exts,
+        .dev_ext_count = ARRAY_SIZE(dev_exts),
+    };
+    vk_init(vk, &vk_params);
 
     /* signal readiness */
     test->run = true;
@@ -209,9 +419,12 @@ android_test_thread(void *arg)
         android_test_handle_state(test);
     }
 
+    vk_cleanup(vk);
+
     if (test->cur.queue)
         AInputQueue_detachLooper(test->cur.queue);
     test->looper = NULL;
+    test->choreo = NULL;
 
     mtx_unlock(&test->mutex);
 
@@ -461,7 +674,11 @@ ANativeActivity_onCreate(ANativeActivity *act, void *savedState, size_t savedSta
 {
     const struct android_test test_templ = {
         .verbose = true,
-        .format = AHARDWAREBUFFER_FORMAT_R8G8B8A8_UNORM,
+        .vk_format = VK_FORMAT_R8G8B8A8_UNORM,
+        .ahb_format = AHARDWAREBUFFER_FORMAT_R8G8B8A8_UNORM,
+        .ahb_usage = AHARDWAREBUFFER_USAGE_GPU_FRAMEBUFFER |
+                     AHARDWAREBUFFER_USAGE_GPU_SAMPLED_IMAGE |
+                     AHARDWAREBUFFER_USAGE_COMPOSER_OVERLAY,
         .act = act,
     };
 
@@ -473,6 +690,26 @@ ANativeActivity_onCreate(ANativeActivity *act, void *savedState, size_t savedSta
     if (test->verbose) {
         android_log("onCreate: sdk %d, internal %s, external %s", act->sdkVersion,
                     act->internalDataPath, act->externalDataPath);
+    }
+
+    if (!(test->ahb_usage & AHARDWAREBUFFER_USAGE_GPU_SAMPLED_IMAGE) ||
+        !(test->ahb_usage & AHARDWAREBUFFER_USAGE_COMPOSER_OVERLAY))
+        android_die("missing sf usage");
+
+    if (test->ahb_usage & AHARDWAREBUFFER_USAGE_PROTECTED_CONTENT) {
+        if ((test->ahb_usage & AHARDWAREBUFFER_USAGE_CPU_READ_MASK) !=
+            AHARDWAREBUFFER_USAGE_CPU_READ_NEVER)
+            android_die("protected with cpu read");
+        if ((test->ahb_usage & AHARDWAREBUFFER_USAGE_CPU_WRITE_MASK) !=
+            AHARDWAREBUFFER_USAGE_CPU_WRITE_NEVER)
+            android_die("protected with cpu write");
+        if (!(test->ahb_usage & AHARDWAREBUFFER_USAGE_GPU_FRAMEBUFFER))
+            android_die("protected without gpu fb");
+    } else {
+        if (!(test->ahb_usage & AHARDWAREBUFFER_USAGE_GPU_FRAMEBUFFER) &&
+            (test->ahb_usage & AHARDWAREBUFFER_USAGE_CPU_WRITE_MASK) ==
+                AHARDWAREBUFFER_USAGE_CPU_WRITE_NEVER)
+            android_die("no cpu or gpu write");
     }
 
     ANativeActivityCallbacks *cbs = act->callbacks;
